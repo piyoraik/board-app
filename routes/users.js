64bd35fec613d0e938649a386cfaa0da5e07e1fb
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-09-16 01:28:26 +0900

    [UPDATE]登録画面で登録できなかった場合、エラーメッセージを表示

--- routes/users.js
+++ routes/users.js
@@ -15,23 +15,33 @@ router.get('/', (req, res, next) => {
 
 router.get('/add', (req, res, next) => {
   var data = {
-    title: 'Users/Add'
+    title: 'Users/Add',
+    form: new db.User(),
+    err: null
   }
   res.render('users/add', data);
 });
 
 router.post('/add', (req, res, next) => {
+  const form = {
+    name: req.body.name,
+    pass: req.body.pass,
+    mail: req.body.mail,
+    age: req.body.age
+  }
   db.sequelize.sync()
-    .then(() => db.User.create({
-      name: req.body.name,
-      pass: req.body.pass,
-      mail: req.body.mail,
-      age: req.body.age
-    }))
-    .then(user => {
+    .then(() => db.User.create(form))
+    .then(() => {
       res.redirect('/users');
-    });
-
+    })
+    .catch(err => {
+      var data = {
+        title: 'User/Add',
+        form: form,
+        err: err
+      }
+      res.render('users/add', data);
+    })
 });
 
 router.get('/edit', (req, res, next) => {
