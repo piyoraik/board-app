e78b7fcb6f11c87558f45c123b5ab65854bd838d
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-09-16 00:15:51 +0900

    [ADD]Edit、Updateの実装

--- routes/users.js
+++ routes/users.js
@@ -34,4 +34,34 @@ router.post('/add', (req, res, next) => {
 
 });
 
+router.get('/edit', (req, res, next) => {
+  db.User.findByPk(req.query.id)
+    .then(user => {
+      var data = {
+        title: 'Users/Edit',
+        form: user
+      }
+      res.render('users/edit', data);
+    });
+});
+
+router.post('/edit', (req, res, next) => {
+  db.sequelize.sync()
+    .then(() => db.User.update({
+      name: req.body.name,
+      pass: req.body.pass,
+      mail: req.body.mail,
+      age: req.body.req
+    },
+      {
+        where: { id: req.body.id }
+      }))
+    .catch((error) => {
+      console.log(error);
+    })
+    .then(user => {
+      res.redirect('/users')
+    });
+});
+
 module.exports = router;
